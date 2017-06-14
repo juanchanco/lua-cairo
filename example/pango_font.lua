2227e581209adeb1789e0ee6fd5ff32568925a89
Juan Chanco <juan.chanco@bluestembrands.com>
2017-06-14 08:11:23 -0500

    iterator in lua

--- example/pango_font.lua
+++ example/pango_font.lua
@@ -3,4 +3,19 @@ local Cairo = require("Cairo")
 local map = Cairo.getDefaultFontMap()
 local families = map:listFamilies()
 print(#families)
-print(families[0])
+local iter = function(m)
+  local i = 0
+  local f = function()
+    i = i + 1
+    local v = m[i]
+    if (v) then
+      return i,m[i]
+    else
+      return nil
+    end
+  end
+  return f
+end
+for i,v in iter(families) do
+  print(string.format("(%i,%s)", i, v:getName()))
+end
